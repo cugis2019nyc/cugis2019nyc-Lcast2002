cddd18b986666a31757357d8710ce888683534bb
Lcast2002 <54040245+Lcast2002@users.noreply.github.com>
2019-08-13 15:01:33 -0400

    Add files via upload

---
+++ tapas first code day 2.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+print("hello world")
+print("20021105")
+print("hello world") #greeting
+print("lydmarie")
+5*2
+5/2
+5-2
+5^2
+8/9*3
+(5)^2
+5*2
+5/2
+5-2
+5**2
+8/9*3
+def add(a,b):
+    add = a+b
+    print(add)
+add(2,7)
+def add(a,b,c):
+    add = a+b+c
+    print(add)
+    
+add(9,100,1)
+print("the sum od 9,100 and 1 is =")
+print("the sum of a = 9")
+print("the sum of b = 100")
+print("the sum of c = 1")
+def add(a,b,c):
+    add = a+b+c
+    print(add)
+add(2,3,4)
+def multiply(a,b,c):
+    multiply = a*b*c
+    print(multiply)
+multiply(1,3,1)
+
+def add(a,b,c):
+    add = a+b+c
+    print("the product of",a,b,"and",c,"is", add)
+
+add(2,3,4)
+
+def add(a,b,c):
+    add = a+b+c
+    print("the product of",a,b,"and",c,"is", add)
+
+def area(b,h):
+    area = 1/2*b*h
+    print(area)
+    
+area(1,2)
+
+def area(b,h):
+    area = 1/2*b*h
+    print("the area of a triangle with a base of",b,"and height of",h,"is", area)
+    
+area(1,2)
+print("milk,white,dark")
+chocolate1 = "milk"
+chocolate2 = "white"
+chocolate3 = "dark"
+print(chocolate1)
+print(chocolate2)
+print(chocolate3)
+amountofmilk = 7
+
+newamount = 8
+
+amountofmilk += newamount
+
+print(amountofmilk)    
+amountofwhite = 8
+amountofdark = 6
+
+amountofmilk +3
+amountofwhite +2
+amountofdark +2
+
+import math
+dir(math)
+math.factorial(100)
+math.sqrt(144)
+math.exp(2)
+math.exp(1)
+2.718281828459045**2
+math.pow(2.71828,2)
